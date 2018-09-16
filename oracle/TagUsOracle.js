fb9ccf36f6d65f8f42781215030d2eb4f6f36ed1
prc <prcolaco@gmail.com>
2018-09-16 08:26:40 +0100

    Added database config, connection and dependencies

--- oracle/TagUsOracle.js
+++ oracle/TagUsOracle.js
@@ -1,9 +1,16 @@
-const Offer = require('./models/Offer');
+const mongoose = require('mongoose');
 const Qtum = require('qtumjs').Qtum;
+const Offer = require('./models/Offer');
+const config = require('./config.json');
 
+// Connect to the mongodb database
+mongoose.connect(config.database);
+
+// Initialise QTUM blockchain connection
 const repoData = require("./solar.development.json");
 const qtum = new Qtum("http://qtum:test@localhost:3889", repoData);
 
+// Load contract information
 const contract = qtum.contract("contracts/TagUsTransaction.sol");
 
 // Listen to events of type OfferPublished
